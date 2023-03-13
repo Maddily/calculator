f3bfa55c00fffaa6840414e5bf59dd084b812b02
Maddie <115709272+Maddily@users.noreply.github.com>
2023-03-13 19:32:30 +0200

    Create a function to operate on two operands based on operator

--- index.js
+++ index.js
@@ -17,3 +17,21 @@ function divide(n1, n2) {
 function remainder(n1, n2) {
     return n1 % n2;
 }
+
+function operate(operator, n1, n2) {
+    if (operator == '+') {
+        return add(n1, n2);
+    }
+    else if (operator == '–') {
+        return subtract(n1, n2);
+    }
+    else if (operator == '×') {
+            return multiply(n1, n2);
+    }
+    else if (operator == '÷') {
+            return divide(n1, n2);
+    }
+    else if (operator == '%') {
+        return remainder(n1, n2);
+    }
+}
